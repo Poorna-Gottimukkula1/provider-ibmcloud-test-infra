2b203eb0e06764736ee4b597aa90e5e42b312c71
Ananya Kumari <Ananya.Kumari@ibm.com>
2026-03-05 13:58:39 +0530

    Update main.tf line 108-112

--- kubetest2-tf/data/powervs/main.tf
+++ kubetest2-tf/data/powervs/main.tf
@@ -105,8 +105,8 @@ EOT
 
 resource "null_resource" "generate_instance_list_json" {
   provisioner "local-exec" {
-    command = <<EOT    
-echo "${data.template_file.instance_list_template.rendered}" > ${path.root}/instance_list.json
+    command = <<-EOT    
+echo '${data.template_file.instance_list_template.rendered}' > ${path.root}/instance_list.json
 cat ${path.root}/instance_list.json
 ls -la ${path.root}/instance_list.json
 EOT
